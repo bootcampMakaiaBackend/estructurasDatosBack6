c6a7fd7af5c76cb07e0366fcb17b1ef55b67f9c5
mateo <mateozacar@gmail.com>
2024-05-21 08:01:53 -0500

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,31 @@
+public class LinkedList {
+    // un nodo []->
+    class Node {
+        int value;
+        Node next = null;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    //Head
+    protected Node head = null;
+    //Tail
+    protected Node tail = null;
+
+    //[8] -> head > tail
+    //[7] -> [8]
+    //head    tail
+    public void addHead(int value){
+        Node newNode = new Node(value);
+        newNode.next = head; //null
+        head = newNode;
+        //que pasa cuandola lista esta vacia?
+        if(newNode.next == null){
+            tail=newNode;
+        }
+    }
+
+    public void addTail(int valor){
+    
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       LinkedList linkedList = new LinkedList();
+       linkedList.addHead(0);
+       linkedList.addHead(123);
+       linkedList.addHead(124);
+       linkedList.addHead(66);
+       linkedList.addHead(66);
+       linkedList.addHead(77);
+        System.out.println("asda");
+    }
+}
