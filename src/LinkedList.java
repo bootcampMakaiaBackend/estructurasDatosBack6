fae2effed7cfb715ba2a6d695ab738c2971bf91f
mateo <mateozacar@gmail.com>
2024-05-21 18:55:24 -0500

    task 2

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -11,25 +11,52 @@ public class LinkedList {
     protected Node head = null;
     //Tail
     protected Node tail = null;
+    //[valor] next -> [valor]
 
-    //[8] -> head > tail
-    //[7] -> [8]
-    //head    tail
+    //9
+    //[5] ->  [8] -> [9] ->null
+    //head          tail
     public void addHead(int value){
-        Node newNode = new Node(value);
-        newNode.next = head; //null
-        head = newNode;
+        Node newNode = new Node(value);  //[5] .next -> null
+        newNode.next = head; // [5] .next -> [8]
+        head = newNode; // [5] -> head
         //que pasa cuandola lista esta vacia?
         if(newNode.next == null){
             tail=newNode;
         }
     }
+    //
+    public void addTail(int valor) {
+        if(tail == null) {
 
-    public void addTail(int valor){
+        }
+    }
+
+    // index - > 2 el valor 6
+    //[5] ->  [8] ->  [6] ->  [9]  -> [78] -> [123] -> [70] -> [800]
+    public void addByPosition(int index, int value) {
+        //index = 0 y el valor 5
+        if(index == 0) {
+            addHead(value);
+        }
+        if(index < 0){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void deleteByPosition(int index, int value) {
 
     }
 
-    public void addByPosition(int index, int value){
+    public void delete() {
 
+    }
+
+    public Node getNodeByIndex(int index) {
+        return null;
+    }
+
+    public int getValueByIndex(int index) {
+        return 0;
     }
 }
